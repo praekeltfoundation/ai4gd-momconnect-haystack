dab79a66064b86372229f76a4ec7005388c3454c
Rudi Giesler <rudi@praekelt.org>
2025-06-03 17:01:58 +0200

    Add token auth to API

--- src/ai4gd_momconnect_haystack/api.py
+++ src/ai4gd_momconnect_haystack/api.py
@@ -1,7 +1,8 @@
 from os import environ
-from dotenv import load_dotenv
+from typing import Annotated
 
-from fastapi import FastAPI
+from dotenv import load_dotenv
+from fastapi import Depends, FastAPI, Header, HTTPException
 
 load_dotenv()
 API_TOKEN = environ["API_TOKEN"]
@@ -12,3 +13,28 @@ app = FastAPI()
 @app.get("/health")
 def health():
     return {"health": "ok"}
+
+
+def verify_token(authorization: Annotated[str, Header()]):
+    """
+    Verify the API token from the Authorization header.
+    """
+    scheme, _, credential = authorization.partition(" ")
+    if scheme.lower() != "token":
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid authentication format. Expected 'Token <token>'",
+        )
+
+    if credential != API_TOKEN:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid authentication token",
+        )
+
+    return credential
+
+
+@app.post("/v1/onboarding")
+def onboarding(token: str = Depends(verify_token)):
+    return {}
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,8 +1,52 @@
+import os
+from unittest import mock
+
 from fastapi.testclient import TestClient
+
 from ai4gd_momconnect_haystack.api import app
+
 
 def test_health():
     client = TestClient(app)
     response = client.get("/health")
     assert response.status_code == 200
     assert response.json() == {"health": "ok"}
+
+
+@mock.patch.dict(os.environ, {"API_TOKEN": "testtoken"}, clear=True)
+def test_onboarding_missing_auth():
+    client = TestClient(app)
+    response = client.post("/v1/onboarding")
+    assert response.status_code == 422
+
+
+@mock.patch.dict(os.environ, {"API_TOKEN": "testtoken"}, clear=True)
+def test_onboarding_invalid_auth_value():
+    client = TestClient(app)
+    response = client.post("/v1/onboarding", headers={"Authorization": ""})
+    assert response.status_code == 401
+
+
+@mock.patch.dict(os.environ, {"API_TOKEN": "testtoken"}, clear=True)
+def test_onboarding_invalid_auth_scheme():
+    client = TestClient(app)
+    response = client.post(
+        "/v1/onboarding", headers={"Authorization": "Bearer testtoken"}
+    )
+    assert response.status_code == 401
+
+
+@mock.patch.dict(os.environ, {"API_TOKEN": "testtoken"}, clear=True)
+def test_onboarding_invalid_auth_token():
+    client = TestClient(app)
+    response = client.post("/v1/onboarding", headers={"Authorization": "Token invalid"})
+    assert response.status_code == 401
+
+
+@mock.patch.dict(os.environ, {"API_TOKEN": "testtoken"}, clear=True)
+def test_onboarding():
+    client = TestClient(app)
+    response = client.post(
+        "/v1/onboarding", headers={"Authorization": "Token testtoken"}
+    )
+    assert response.status_code == 200
